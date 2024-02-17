18eaf36b914a6ed9dcb250bdbac2c4894e69f4bd
Masumi Yano <92832478+MasumiYano@users.noreply.github.com>
2024-02-16 22:47:37 -0800

    Question one to five done.

--- src/process_image.py
+++ src/process_image.py
@@ -3,48 +3,55 @@ import numpy as np
 
 def get_pixel(im, col, row, channel):
     height, width = im['data'].shape[0], im['data'].shape[1]
-    selected_col = 0
-    selected_row = 0
-    if col > height:
-        selected_col = height
-    elif row > width:
-        selected_row = width
+    if col < 0 or row < 0:
+        selected_col = 0
+        selected_row = 0
+    elif col > height or row > width:
+        selected_col = height - 1
+        selected_row = width - 1
     else:
-        selected_col = col
-        selected_row = row
+        selected_col = col - 1 if col != 0 else col
+        selected_row = row - 1 if row != 0 else row
     return im['data'][selected_row, selected_col][channel]
 
 
 def set_pixel(im, col, row, channel, value):
-    existing_value = get_pixel(im, col, row, channel)
-    existing_value = value
+    im['data'][row, col][channel] = value
 
 
 def make_image(width, height, channel):
-    return {'data': np.zeros((height, width, channel), dtype=np.float32), 'w': width, 'h': height, 'c': channel}
+    if channel == 1:
+        return {'data': np.zeros((height, width), dtype=np.float32), 'w': width, 'h': height, 'c': channel}
+    else:
+        return {'data': np.zeros((height, width, channel), dtype=np.float32), 'w': width, 'h': height, 'c': channel}
 
 
 def copy_image(im):
+    pixel_arr = [pixel for pixel in im['data']]
     copy = make_image(im['w'], im['h'], im['c'])
-    # TODO Fill this in
+    copy['data'] = np.array(pixel_arr)
     return copy
 
 
 def rgb_to_grayscale(im):
     assert im['c'] == 3
     gray = make_image(im['w'], im['h'], 1)
-    # TODO Fill this in
+    for row in range(im['h']):
+        for col in range(im['w']):
+            R = im['data'][row, col, 0]
+            G = im['data'][row, col, 1]
+            B = im['data'][row, col, 2]
+            luma = (0.229 * R) + (0.587 * G) + (0.114 * B)
+            gray['data'][row, col] = luma
     return gray
 
 
-def shift_image(im, c, v):
-    # TODO Fill this in
-    pass
-
-
-def clamp_image(im):
-    # TODO Fill this in
-    pass
+def shift_image(im, channel, value):
+    factor = 1 + value
+    for row in range(im['h']):
+        for col in range(im['w']):
+            value = im['data'][row, col, channel]
+            im['data'][row, col, channel] = value * factor
 
 
 # These might be handy
--- tryit.py
+++ tryit.py
@@ -1,39 +1,40 @@
 from src import prep_image, process_image, args
 
-# 1. Getting and setting pixels
-im = prep_image.load_image("data/dog.jpg")
-print(f"(Height, Width, Channel): {im['data'].shape}")
-print(im['c'])
-# print(im['data'])
-total_num = 0
-for row in range(im['h']):
-    for col in range(im['w']):
-        total_num += 1
-        print(im['data'][row, col])
-#         process_image.set_pixel(im, col, row, 0, 0)
-# prep_image.save_image(im, "dog_no_red")
-
-
+# # 1. Getting and setting pixels
+# im_1 = prep_image.load_image("data/dog.jpg")
+# total_num = 0
+# for row in range(im_1['h']):
+#     for col in range(im_1['w']):
+#         process_image.set_pixel(im_1, col, row, 0, 0)
+# prep_image.save_image(im_1, "dog_no_red")
+#
+#
+# # 2. Copying an existing image.
+# im_2 = prep_image.load_image('data/dog.jpg')
+# im_2_copy = process_image.copy_image(im_2)
+# prep_image.save_image(im_2_copy, 'copy_dog')
+#
 # # 3. Grayscale image
-# im = load_image("data/colorbar.png")
-# graybar = rgb_to_grayscale(im)
-# save_image(graybar, "graybar")
-#
-# # 4. Shift LoadImage
-# im = load_image("data/dog.jpg")
-# shift_image(im, 0, .4)
-# shift_image(im, 1, .4)
-# shift_image(im, 2, .4)
-# save_image(im, "overflow")
-#
+# im_3 = prep_image.load_image("data/colorbar.png")
+# graybar = process_image.rgb_to_grayscale(im_3)
+# prep_image.save_image(graybar, "graybar")
+
+
+# 4. Shift LoadImage
+im_4 = prep_image.load_image("data/dog.jpg")
+process_image.shift_image(im_4, 0, .4)
+process_image.shift_image(im_4, 1, .4)
+process_image.shift_image(im_4, 2, .4)
+prep_image.save_image(im_4, "overflow")
+
 # # 5. Clamp LoadImage
-# clamp_image(im)
-# save_image(im, "doglight_fixed")
+# clamp_image(im_1)
+# save_image(im_1, "doglight_fixed")
 #
 # # 6-7. Colorspace and saturation
-# im = load_image("data/dog.jpg")
-# rgb_to_hsv(im)
-# shift_image(im, 1, .2)
-# clamp_image(im)
-# hsv_to_rgb(im)
-# save_image(im, "dog_saturated")
+# im_1 = load_image("data/dog.jpg")
+# rgb_to_hsv(im_1)
+# shift_image(im_1, 1, .2)
+# clamp_image(im_1)
+# hsv_to_rgb(im_1)
+# save_image(im_1, "dog_saturated")
